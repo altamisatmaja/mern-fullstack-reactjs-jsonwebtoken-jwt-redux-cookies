60786cdf8a676a9ef9f38b15654918b83c1e1e1b
altamisatmaja <altamisfattah16@gmail.com>
2024-01-23 00:13:42 +0700

    feat: api, cors, ui sign up

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+    {
+      message: "User created succesfully";
+    }
+  };
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Sign Up</h1>
+      <form
+        onSubmit={handleSubmit}
+        action="post"
+        className="flex flex-col gap-4"
+      >
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Masukkan username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Masukkan email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Masukkan password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button className="bg-slate-700 p-3 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5 ">
+        <p>
+          Have an accout?
+          <Link to="/sign-in">
+            <span className="text-blue-500">Sign In</span>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
