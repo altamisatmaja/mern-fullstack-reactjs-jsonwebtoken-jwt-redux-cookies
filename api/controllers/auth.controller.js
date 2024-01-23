a9455e1f535056c7e42ad507473c115814a50f8e
altamisatmaja <altamisfattah16@gmail.com>
2024-01-23 07:03:27 +0700

    feat: add credentials

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptj from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,3 +16,16 @@ export const signup = async (req, res, next) => {
   }
 };
 
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptj.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, proccess.env.JWT_SECRET);
+  } catch (error) {
+    next(error);
+  }
+};
